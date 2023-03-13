0ebd4ad63b076a5a572ec265e2873fc1a45c480f
unknown <amnd.castro@gmail.com>
2023-03-13 19:32:34 -0300

    vs7

--- If-Else/Exercicio-1-aula-2.py
+++ If-Else/Exercicio-1-aula-2.py
@@ -4,3 +4,18 @@ est1 = input("Insira a 1º estatura:")
 est2 = input("Insira a 2º estatura:")
 est3 = input("Insira a 3º estatura:")
 
+#Comparação, revela a maior estatura
+if est1 == est2 or est2 == est3 or est1 == est3:
+    exit() #encerra programa 
+elif est1 > est2 and est1 > est3:
+    print("a pessoa de estatura n° 1 é mais alta")
+elif est2 > est1 and est2 > est3:
+    print("a pessoa de estatura n° 2 é mais alta")
+elif est3 > est1 and est3 > est2:
+    print("a pessoa de estatura n° 3 é mais alta")
+
+#cria array estatura e printa com o método sort em ordem crescente
+#array.sort(reverse=True) -> usa o reverse para ordenar decrescente
+estatura = [est1, est2, est3]
+estatura.sort(reverse=True)
+print(estatura)
--- If-Else/Exercicio-2-aula-2.py
+++ If-Else/Exercicio-2-aula-2.py
@@ -1,1 +1,15 @@
-#Construa um programa que solicite ao usuário dois números positivos. Em seguida, o programa deve apresentar o seguinte menu.1  Média ponderada, com pesos 2 e 3, respectivamente2. Quadrado da soma dos 2 números 3. Cubo do menor número Escolha uma opção:De acordo com a opção informada, o programa deve calcular a operação apresentada no menu. Se a opção escolhida for inválida, o programa deve mostrar a mensagem “Opção inválida” e ser encerrado. 
+#Construa um programa que solicite ao usuário dois números positivos. Em seguida, o programa deve apresentar o seguinte menu.1  Média ponderada, com pesos 2 e 3, respectivamente / 2. Quadrado da soma dos 2 números / 3. Cubo do menor número / Escolha uma opção:De acordo com a opção informada, o programa deve calcular a operação apresentada no menu. Se a opção escolhida for inválida, o programa deve mostrar a mensagem “Opção inválida” e ser encerrado. 
+
+print("\n1. Média ponderada \n2. Quadrado da soma dos 2 números \n3. Cubo do menor número\n ")
+
+i = int(input("Escolha a opção: "))
+
+while i != 0:
+    if i == 1:
+        print()
+    elif i == 2:
+        print()
+    elif i == 3:
+        print()
+    else:
+        exit()
